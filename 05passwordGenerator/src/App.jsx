6cb1bd1e811a11e757c1ec058a5149d34bf43779
Digilearning000 <digilearning000@gmail.com>
2023-11-26 10:27:43 +0530

    Pass Generator left in Mid

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useCallback } from "react";
+import "./App.css";
+
+function App() {
+  // const [length, setLength] = useState(8);
+
+  // const [numAllowed, setNumAllowed] = useState(false);
+  // const [charAllowed, setCharAllowed] = useState(false);
+  // const [password, setPassword] = useState("");
+
+  // const passGenerator = useCallback(fn, []);
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
